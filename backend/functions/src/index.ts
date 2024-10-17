0debd553eb762311e2127d4ec36515be0f2e9a00
Nick <maniutin@gmail.com>
2024-10-16 22:38:52 -0400

    add create, update and delete event functions

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -57,3 +57,62 @@ exports.getEventById = onRequest(async (req: any, res: any) => {
     res.status(500).send(error);
   }
 });
+
+// Add a new event
+// example string: http://127.0.0.1:5001/panda-hub-a4da9/us-central1/createEvent?title=Event%20Title&description=Event%20Description&location=Event%20Location&organizer=Event%20Organizer&eventType=Event%20Type&date=2024-12-25
+exports.createEvent = onRequest(async (req: any, res: any) => {
+  // TODO: cleanup types
+  const { title, description, date, location, organizer, eventType } = req.body;
+  try {
+    const event = {
+      title,
+      description,
+      date: Timestamp.fromDate(new Date(date)),
+      location,
+      organizer,
+      eventType,
+      updatedAt: FieldValue.serverTimestamp(),
+    };
+
+    const docRef = await db.collection("events").add(event);
+    res.json({ result: `Message with ID: ${docRef.id} added.` });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Update an event
+exports.updateEvent = onRequest(async (req: any, res: any) => {
+  // TODO: cleanup types
+  const { id, title, description, date, location, organizer, eventType } =
+    req.body;
+
+  try {
+    const updatedEvent = {
+      title,
+      description,
+      date: Timestamp.fromDate(new Date(date)),
+      location,
+      organizer,
+      eventType,
+      updatedAt: FieldValue.serverTimestamp(),
+    };
+    await db.collection("events").doc(id).update(updatedEvent);
+    res.status(200).send("Event updated successfully");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Delete an event
+exports.deleteEvent = onRequest(async (req: any, res: any) => {
+  // TODO: cleanup types
+
+  const { id } = req.body;
+  try {
+    await db.collection("events").doc(id).delete();
+    res.status(200).send("Event deleted successfully");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
