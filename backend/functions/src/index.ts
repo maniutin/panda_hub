bd6b899b2a674360a705646363185e20d24de0e8
Nick <maniutin@gmail.com>
2024-10-16 22:38:07 -0400

    add getAllEvents and getEventById functions

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,19 +1,59 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { functions, logger } = require("firebase-functions");
+const { onRequest } = require("firebase-functions/v2/https");
+const {
+  onDocumentCreated,
+  onDocumentWritten,
+  Change,
+  FirestoreEvent,
+} = require("firebase-functions/v2/firestore");
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const {
+  admin,
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+  Filter,
+} = require("firebase-admin/firestore");
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+initializeApp();
+
+const db = getFirestore();
+db.settings({ ignoreUndefinedProperties: true });
+
+// Get all events
+exports.getAllEvents = onRequest(async (req: any, res: any) => {
+  // TODO: cleanup types
+
+  try {
+    const eventsSnapshot = await db.collection("events").get();
+    const events = eventsSnapshot.docs.map((doc: any) => ({
+      id: doc.id,
+      ...doc.data(),
+    })); // TODO: cleanup types
+
+    res.status(200).json(events);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Get an event by ID
+exports.getEventById = onRequest(async (req: any, res: any) => {
+  // TODO: cleanup types
+  const { id } = req.query; // change req.query to req.body when sending req from client
+  try {
+    const doc = await db.collection("events").doc(id).get();
+    if (!doc.exists) {
+      return res.status(404).send("Event not found");
+    }
+    res.status(200).json({ id: doc.id, ...doc.data() });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
